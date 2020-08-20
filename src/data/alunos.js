273e76ba09bf961c4ff8276606ea8feff1b6a75f
Fernando Brum <dev.fobrum@gmail.com>
2020-08-20 19:13:06 -0300

    first commit

---
+++ src/data/alunos.js
@@ -0,0 +1,7 @@
+export default [
+    { id: 1, nome: 'Fernando', nota: 8.3 },
+    { id: 2, nome: 'Camila', nota: 6.7 },
+    { id: 3, nome: 'Junior', nota: 5.5 },
+    { id: 4, nome: 'Luiz', nota: 5.8 },
+    { id: 5, nome: 'Roberta', nota: 10.0 },
+]
---
+++ src/data/produtos.js
@@ -0,0 +1,10 @@
+const produtos = [
+    { id: 1, nome: 'Caneta', preco: 1.99 },
+    { id: 2, nome: 'Lapis', preco: 0.99 },
+    { id: 3, nome: 'iPad', preco: 4499.99 },
+    { id: 4, nome: 'Sansung S20 Ultra', preco: 4599.99 },
+    { id: 5, nome: 'Notebook', preco: 4409.99 },
+    { id: 6, nome: 'Caderno', preco: 59.99 },
+]
+
+export default produtos
